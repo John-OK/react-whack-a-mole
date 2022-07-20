1ad48065cadf7d284d11511b555b2bc9d13ffebd
John-OK <john.ok.esp@gmail.com>
2022-07-19 19:42:11 -0500

    Made a kick ass mole whacker. TODO: Get difficulty level slector to work right.

--- src/App.js
+++ src/App.js
@@ -2,45 +2,81 @@ import './App.css'
 import React, { useEffect, useState } from 'react'
 import Mole from './components/mole/Mole.js'
 
+const [EASY, NORMAL, HARD, INSANE] = [2000, 1500, 1000, 50]
+
+
 function App() {
   // states
   const [dens, setDens] = useState(getDensState())
   const [points, setPoints] = useState(0)
+  const [difficulty, setDifficulty] = useState(NORMAL)
 
   // effects
   useEffect(() => {
     startGame()
-  }, [])
+  }, [difficulty])
 
   // helpers
+
   function startGame() {
+    console.log(difficulty)
     setInterval(() => {
       setDens(getDensState())
-    }, 1500)
+    }, difficulty)
+    console.log(difficulty)
+  }
+
+  function clickHandler(e) {
+    setDifficulty(+(e.target.value))
+    console.log(difficulty)
   }
 
   function getDensState() {
     return new Array(9).fill({}).map(() => {
-      return { 
-        isMoleVisible: [true,false][Math.round(Math.random())] 
+      return {
+        isMoleVisible: [true,false][Math.round(Math.random())]
       }
     })
   }
 
   function onMoleWhacked() {
     setPoints(points + 1)
+    console.log("You hit a mole!")
   }
+
+  function onMissMole() {
+    console.log("You missed!")
+  } 
 
   // renders
   const denElements = dens.map((den, index) => {
+    let func = onMissMole
+    if (den['isMoleVisible']) {
+      func = onMoleWhacked
+    }
     return (
-      <Mole key={`mole-${index}`} />
+      <Mole den={den} func={func} key={`mole-${index}` }
+      />
     )
   })
 
   return (
     <div className="App">
       <h1>WHACK-A-MOLE!</h1>
+      <select value={difficulty} onChange={clickHandler}>Difficulty:
+        <option value={EASY}>
+          Easy
+        </option>
+        <option value={NORMAL}>
+          Normal
+        </option>
+        <option value={HARD}>
+          Hard
+        </option>
+        <option value={INSANE}>
+          Insane
+        </option>
+      </select>
       <h2>Points: { points }</h2>
       <div className="dens">
         { denElements }
--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -1,10 +1,13 @@
 import './Mole.css'
 import MoleIcon from './Mole.svg'
+import onMoleWhacked from '../../App.js'
+
 
 function Mole(props) {
   return (
     <div className="den">
-      <img src={MoleIcon} className="Mole" alt="Mole" />
+      {/* <img src={props.den['isMoleVisible'] ? MoleIcon : "Mole"} className="Mole" alt="Mole" onClick={props.func}/> */}
+      <img src={props.den['isMoleVisible'] ? MoleIcon : "Mole"} className={props.den['isMoleVisible'] ? "Mole" : "Mole Empty"} alt="Mole" onClick={props.func}/>
     </div>
   )
 }
